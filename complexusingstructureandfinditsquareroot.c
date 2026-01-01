ae5cd27135b687d837de18fb41713af2e9be6046
danish khan <danishbiss6@gmail.com>
2026-01-01 14:21:51 +0530

    Add complex number structure and square root function
    Implement a structure for complex numbers and a function to calculate their square root.

---
+++ complexusingstructureandfinditsquareroot.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+struct complex
+{
+ int real ;
+ int  img ;
+};
+void squareroot (struct complex c1){
+  real = ptr ->real ; 
+  img = ptr-> img ; 
+printf("magnitude of ",sqrt() )
+}
+
+int main ()
+{
+struct complex c1 ={8,8};
+ struct complex *ptr = &c1 ; 
+ printf("%d,%d\n",ptr->real , ptr->img);
+
+ return 0 ; 
+}x
